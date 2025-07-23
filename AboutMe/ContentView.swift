ac69f273f2ce89db48a2c36404e873f88c76c9c5
Krisha <akrisha259@gmail.com>
2025-07-23 13:41:28 -0700

    Added image, shaped image, adjust page and text

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "fish")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello!")
+        ZStack() {
+            Color(.systemGray2)
+                .ignoresSafeArea()
+            VStack(alignment: .center) {
+                VStack(spacing: 20) {
+                    Image("catHat")
+                        .resizable()
+                        .frame(width: 175)
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        
+                }
+                //                Image("catHat")
+                //                    .resizable()
+                //                    .aspectRatio(contentMode: .fit)
+                //                    .clipShape(<#T##shape: Shape##Shape#>)
+                Text("Krisha Agarwal")
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
+            .frame(width: 350)
+            .padding()
+            .background(Rectangle() .foregroundColor(.yellow))
+            .cornerRadius(15)
+            .shadow(radius:15)
+            .padding()
         }
-        .padding()
     }
 }
 
